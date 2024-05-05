0773fa399f483ac5e8530abaedf4cda66b953f07
Filip Grzywok <f.grzywok@gmail.com>
2024-05-05 23:15:04 +0200

    feat: Add all archs to build matrix

--- scripts/build-octo4a-bootstrap.sh
+++ scripts/build-octo4a-bootstrap.sh
@@ -7,8 +7,19 @@ set -eu
 readonly O4A_PKGS='alpine-keys apk-tools gcc python3 py3-pip libffi-dev python3-dev musl-dev curl git linux-headers openssh-server p7zip bash unzip ffmpeg ttyd'
 readonly ALPINE_BRANCH='3.19'
 
+case "$ARCH" in
+"aarch64")
+    ALPINE_ARCH="aarch64";;
+"armv7a")
+    ALPINE_ARCH="armhf";;
+"i686")
+    ALPINE_ARCH="x86";;
+"x86_64")
+    ALPINE_ARCH="x86_64";;
+esac
+
 sudo sh scripts/alpine-make-rootfs.sh \
-    --arch $ARCH \
+    --arch $ALPINE_ARCH \
     --branch $ALPINE_BRANCH \
     --packages "$O4A_PKGS" \
     --script-chroot \
